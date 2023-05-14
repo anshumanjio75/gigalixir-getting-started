1876b22b285e5f2368efe09e1962861233525b83
Anshuman <anshumanjio75@gmail.com>
2023-05-14 06:24:44 +0530

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -27,7 +27,7 @@ config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
 # which you should run after static files are built and
 # before starting your production server.
 config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [nil, port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
